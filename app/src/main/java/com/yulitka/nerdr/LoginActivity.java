cc2e2ba2198d106efe85049a93b6dfdc0a134c38
yucchan <gazerock13@gmail.com>
2018-04-13 22:57:16 +0300

    added toast on login, register and information btn clicks

--- app/src/main/java/com/yulitka/nerdr/LoginActivity.java
+++ app/src/main/java/com/yulitka/nerdr/LoginActivity.java
@@ -1,7 +1,15 @@
 package com.yulitka.nerdr;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +17,27 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final Button loginBtn = findViewById(R.id.liLoginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),R.string.available_soon,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final TextView registrationBtn = findViewById(R.id.liRegistrationLink);
+        registrationBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),R.string.available_soon,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final ImageView infoImgBtn = findViewById(R.id.liInfoImage);
+        infoImgBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),R.string.available_soon,Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
